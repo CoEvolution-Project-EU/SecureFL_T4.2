eb58919f7a82ff4255fc1089807055e80921b988
Emmanouil Kritharakis <kritharakismanolis@gmail.com>
2026-05-20 11:01:35 +0300

    chore: align run_experiments.sh with single-config pipeline

--- run_experiments.sh
+++ run_experiments.sh
@@ -12,15 +12,13 @@ do
     do
 
       echo "Handling no attack under strategy: $s for dataset: $d with $m malicious users"
-      YAML_FILE="configs/config_no_attack.yaml"
-      yq -i -y ".server.strategy = \"$s\" | .model.name = \"$d\"" "$YAML_FILE"
-      export config_file_name="config_no_attack"
+      YAML_FILE="config.yaml"
+      yq -i -y ".server.strategy = \"$s\" | .model.name = \"$d\" | .attack.type = null" "$YAML_FILE"
       poetry run simulation
 
       echo "Handling Sign Flip attack under strategy: $s for dataset: $d with $m malicious users"
-      YAML_FILE="configs/config_data_attack.yaml"
+      YAML_FILE="config.yaml"
       yq -i -y ".server.strategy = \"$s\" | .attack.num_malicious_clients = $m | .model.name = \"$d\" | .attack.type = \"Sign Flip\"" "$YAML_FILE"
-      export config_file_name="config_data_attack"
       poetry run simulation
 
     done
